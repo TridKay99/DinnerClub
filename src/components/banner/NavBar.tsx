36992d47a7ad0726bf22791edcf24b701b5e52d2
Tristan Kay <triddy@Tristans-MacBook-Pro.local>
2019-11-12 22:17:55 +1100

    fixed home page

--- src/components/banner/NavBar.tsx
+++ src/components/banner/NavBar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/component-navbar.scss'
 import moustache from '../styles/moustache.png'
 import {Button} from "semantic-ui-react";
+import instagram from "../styles/instagram.png";
 
 export class NavBar extends React.Component {
 
@@ -10,11 +11,16 @@ export class NavBar extends React.Component {
       <div className={'navbar'}>
         <div className={'bnavButtons'}>
         <Button className={'btn draw-border'} color={'teal'}>
-          Breakfast Blogs
+          Breakky Blogs
         </Button>
         <Button className={'btn draw-border'} color={'teal'}>
           Dinner Dramas
         </Button>
+        </div>
+        <div className={'instagram'}>
+          <a href={'https://www.instagram.com/dinner_club_adventures/?hl=en'} target="_blank">
+            <img src={instagram} alt={''} className={'instagram'}/>
+          </a>
         </div>
         <div className={'moustacheSpace'}>
           <img src={moustache} alt={''} className={'moustache'}/>
