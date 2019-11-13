802f96287bff3b53ddb0ae37c695f0f369205b92
Tristan Kay <triddy@Tristans-MacBook-Pro.local>
2019-11-13 21:19:24 +1100

    trial blog tiles

--- src/components/blog1Trial.ts
+++ src/components/blog1Trial.ts
@@ -6,7 +6,7 @@ export type BlogInfo = {
 }
 
 
-export const Blog1: BlogInfo = { title: 'A night with the Duck - I Love Dumpling\'s, Richmond',
+export const Blog1: BlogInfo = { title: 'I Love Dumpling\'s, Richmond',
 body: `
 hmmm where do i start?</br>
 <img src="https://res.cloudinary.com/dqsacxogt/image/upload/v1573636967/BlogPhotos/simisJumper_soumcj.jpg" alt="" height='150px' width="auto"/>
@@ -14,4 +14,14 @@ hmmm where do i start?</br>
 this should be a few lines lower xD
 `,
 image: "https://res.cloudinary.com/dqsacxogt/image/upload/v1573636967/BlogPhotos/simisJumper_soumcj.jpg"
+}
+
+export const Blog2: BlogInfo = {
+  title: 'this is another restaurant',
+  body: `
+  this is a trial </br>
+  </br>
+  continuing the trial
+  `,
+  image: 'https://res.cloudinary.com/dqsacxogt/image/upload/v1573636966/BlogPhotos/duckOnDuck_fkvv8y.jpg'
 }
--- src/components/blog_containers/breakky_blog/BreakkyBlogContainer.tsx
+++ src/components/blog_containers/breakky_blog/BreakkyBlogContainer.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import '../../styles/component-blog-containers.scss'
+import {Blog1, Blog2} from "../../blog1Trial";
 
 export class BreakkyBlogContainer extends React.Component {
 
   getBlogTiles = () => {
-    return ''
-  }
+    const blog = Blog1
+    const blog2 = Blog2
+    const blogs = [Blog1, Blog2]
+
+    return blogs.map((blog) => {
+      return (
+        <div className="card">
+          <h3 className="title">{blog.title}</h3>
+          <img src={blog.image} alt={''}/>
+        </div>
+      )
+    })
+  };
 
   render() {
 
     return (
-      <div className={'blogContainer'}>
+      <div className="blogContainer">
         {this.getBlogTiles()}
       </div>
     )
