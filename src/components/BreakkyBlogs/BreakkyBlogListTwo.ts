e71b22fe2af4dc1ac49a9d7bff81030f4155eb84
Tristan Kay <triddy@Tristans-MacBook-Pro.local>
2019-11-25 20:05:08 +1100

    edit for Moby

--- src/components/BreakkyBlogs/BreakkyBlogListTwo.ts
+++ src/components/BreakkyBlogs/BreakkyBlogListTwo.ts
@@ -3,11 +3,11 @@ import {BlogInfo} from "../services/DinnerDramaService";
 export const MobyDick: BlogInfo = {
   title: `Now I'm Moby Dick for real - Moby, Armadale`,
   body: `
-  Great brunch outting this weekend, Moby had the 'eggs your way on toast', yes on toast. We will let it slide!</br>
-  It was a solid 7 only because of the mushys.</br>
+  Great brunch outing this weekend, Moby had the 'eggs your way on toast', yes on toast. We will let it slide!</br>
+  It was a solid 7 only because of the mushies.</br>
   Bread was a tad too hard to cut with my knife, leaving me to flex my guns to get into the pesky crust!</br>
-  Service was a bit slow but they made up for it with their great knowledge of anatomy and the location of the larynx</br>
-  The mimosas where satisfying but it was a shock to my ear holes to find out they didn't have bloody marys.</br>
+  Service was a bit slow but they made up for it with their great knowledge of anatomy and the location of the larynx.</br>
+  The mimosas were satisfying, but it was a shock to my ear holes to find out they didn't have Bloody Marys.
   <img src="https://res.cloudinary.com/dqsacxogt/image/upload/v1574671438/BlogPhotos/simisMimosa_eumarw.jpg" alt="" height='200px' width="auto">
   </br>
   </br>
