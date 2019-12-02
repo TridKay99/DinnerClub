b554c34d9f08b8b6b0fcbada3f5ff8e5208c1fc0
Tristan Kay <triddy@Tristans-MacBook-Pro.local>
2019-12-02 18:24:12 +1100

    new blog: Thai to go.

--- src/components/DinnerBlogs/DinnerDramasListTwo.ts
+++ src/components/DinnerBlogs/DinnerDramasListTwo.ts
@@ -18,3 +18,26 @@ export const TaoDumplings: BlogInfo = {
   `,
   img: "https://res.cloudinary.com/dqsacxogt/image/upload/v1574671470/BlogPhotos/trid_wine_wje9zz.jpg"
 }
+
+export const ThaiToGo: BlogInfo = {
+  title: 'Knick rates our friends out of 10',
+  restaurant: 'Thai Food to go, Windsor',
+  body: `
+  In contrast to yet another cold and wet Friday night, Dinner Club’s taste buds were transported to tropical South East Asia, where we were treated to some delectable Thai.</br>
+  </br>
+  Christian ‘Pad Simi Ew’ Simitzis, once again demanded we get him the Chicky Chicky Pad See Ew just to tickle his sick Pad See Ew needs. He later admitted he has never once 
+  tried any other Thai dish in his life. The devilish grin on his face as he ate was quite sickening, however, his recommendation was spot on and paired exceptionally 
+  well with their Roti and Satay Sauce. Trust me that roti with satay wine and dined me but didn’t fuck me at all.</br>
+  </br>
+  Other dishes of note included the Masaman Curry and the Tom Yum Soup, which nourished not only my body, but my soul. Good use of spice but I feel like they toned it 
+  down as I’m sure in the past many Aussies have complained about it being “too bloody spicy”. Only issue was the mains took too long and made me finish my entire bottle 
+  of wine early.</br>
+  </br>
+  Food – 9/10</br>
+  Weather – 0.4/10</br>
+  Friends – 3/10</br>
+  Below is an example of the 3/10</br>
+  <img src="https://res.cloudinary.com/dqsacxogt/image/upload/v1575271103/BlogPhotos/bobby_kimn4o.jpg" alt="" height='200px' width="auto">
+  `,
+  img: "https://res.cloudinary.com/dqsacxogt/image/upload/v1575271103/BlogPhotos/bobby_kimn4o.jpg"
+}
